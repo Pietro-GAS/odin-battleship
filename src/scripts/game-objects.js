2cd2965d3f89aa8d983d681287d16f3c3c8573ac
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2026-05-05 12:40:24 +0000

    feat: implement Gameboard class

--- src/scripts/game-objects.js
+++ src/scripts/game-objects.js
@@ -13,11 +13,62 @@ export class Ship {
         }
     }
 
+    getHits() {
+        return this.#hits;
+    }
+
+    getLength() {
+        return this.#length;
+    }
+
     isSunk() {
         return this.#hits === this.#length;
     }
 }
 
 export class Gameboard {
-    //
+    #size;
+
+    constructor(size) {
+        this.#size = size;
+        this.board = Array.from( { length: size }, () => Array(size).fill(null));
+    }
+
+    placeShip(x, y, length, orientation="x") {
+        const ship = new Ship(length);
+        if (!this.#validateShip(x, y, ship, orientation)) {
+            throw new Error("Invalid ship placement");
+        }
+
+        if (orientation === "x") {
+            for (let i = 0; i < ship.getLength(); i++) {
+                this.board[y][x + i] = ship;
+            } 
+        } else if (orientation === "y") {
+            for (let i = 0; i < ship.getLength(); i++) {
+                this.board[y + i][x] = ship;
+            }
+        }
+    }
+
+    #validateShip(x, y, ship, orientation) {
+        const positions = [];
+        if (orientation === "x") {
+            if (x + ship.getLength() >= this.#size) {
+                return false;
+            }
+            for (let i = 0; i < ship.getLength(); i++) {
+                positions.push(this.board[y][x + i] === null);
+            } 
+        } else if (orientation === "y") {
+            if (y + ship.getLength() >= this.#size) {
+                return false;
+            }
+            for (let i = 0; i < ship.getLength(); i++) {
+                positions.push(this.board[y + i][x] === null);
+            }
+        }
+
+        return positions.every((element) => element === true);
+    }
 }
--- src/tests/game-objects.spec.js
+++ src/tests/game-objects.spec.js
@@ -1,5 +1,6 @@
 import { Ship, Gameboard } from "../scripts/game-objects.js";
 
+
 describe("Test the Ship class", () => {
     test("Ship exists", () => {
         expect(Ship).toBeDefined();
@@ -17,5 +18,25 @@ describe("Test the Ship class", () => {
 describe("Test the Gameboard class", () => {
     test("Gameboard exists", () => {
         expect(Gameboard).toBeDefined();
+    });
+    test("The gameboard constructor is working", () => {
+        const board = new Gameboard(8).board;
+        expect(board.length).toBe(8);
+        expect(board[0].length).toBe(8);
+    });
+    test("The ship placement method is working", () => {
+        const board = new Gameboard(8);
+        board.placeShip(0, 1, 3); // horizontal
+        expect(board.board[7][7]).toBeNull(); // ship is not here
+        expect(board.board[1][0]).not.toBeNull(); // ship is here
+        expect(board.board[1][1]).not.toBeNull(); // ship is here
+        expect(board.board[1][2]).not.toBeNull(); // ship is here
+        board.placeShip(5, 0, 4, "y"); // vertical
+        expect(board.board[0][5]).not.toBe(null); // ship is here
+        expect(board.board[1][5]).not.toBe(null); // ship is here
+        expect(board.board[2][5]).not.toBe(null); // ship is here
+        expect(board.board[3][5]).not.toBe(null); // ship is here
+        expect(() => { board.placeShip(5, 0, 5) }).toThrow(); // squares already occupied
+        expect(() => { board.placeShip(6, 6, 5) }).toThrow(); // ship is out of bounds
     });
 });
