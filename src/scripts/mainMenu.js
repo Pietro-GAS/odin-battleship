5cf1d99c4d7b99508c907ab87144adc8ac2a9c16
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2026-05-06 13:09:10 +0000

    feat(main menu): add input for board size

--- src/scripts/mainMenu.js
+++ src/scripts/mainMenu.js
@@ -1,13 +1,27 @@
 export const loadMainMenu = () => {
     const body = document.querySelector("body");
-    
+    body.replaceChildren();
+
     const mainMenu = document.createElement("div");
     mainMenu.setAttribute("class", "menu");
     mainMenu.setAttribute("id", "settings-menu");
 
-    const title = document.createElement("h1");
+    const title = document.crea");
     title.setAttribute("class", "heading");
     title.innerHTML = "BATTLESHIP!";
+    
+    const sizeForm = document.createElement("form");
+    sizeForm.setAttribute("id", "size-form");
+
+    const sizeLabel = document.createElement("label");
+    sizeLabel.setAttribute("id", "size-label");
+    sizeLabel.setAttribute("for", "size-input");
+    sizeLabel.innerHTML = "Board size:";
+
+    const sizeInput = document.createElement("input");
+    sizeInput.setAttribute("type", "text");
+    sizeInput.setAttribute("id", "size-input");
+    sizeInput.setAttribute("name", "size-input");
     
     const container = document.createElement("div");
     container.setAttribute("class", "container");
@@ -30,6 +44,9 @@ export const loadMainMenu = () => {
     
     body.appendChild(mainMenu);
     mainMenu.appendChild(title);
+    mainMenu.appendChild(sizeForm);
+    sizeForm.appendChild(sizeLabel);
+    sizeForm.appendChild(sizeInput);
     mainMenu.appendChild(container);
     container.appendChild(btnOneP);
     container.appendChild(btnTwoP);
